734bc40b4ad64fe98ec79f0fde260374793af893
Gabrielle Geffen <gabrielle.geffen@post.runi.ac.il>
2025-11-12 12:49:39 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -9,7 +9,7 @@ public class Cheers {
                         if (anLetters.indexOf(ch) != -1) {
                                 System.out.println("Give me " + "an" + " " + ch + ": " + ch + "!");
                         }
-                        else System.out.println("Give me " + "a" + " " + ch + ": " + ch + "!");
+                        else System.out.println("Give me " + "a " + " " + ch + ": " + ch + "!");
                 }
                 System.out.println("What does that spell?");
                 for (int i = 0; i < times; i++) {
