52b49ec7b069e200271fcb8ee52178241092ee8f
Gabrielle Geffen <gabrielle.geffen@post.runi.ac.il>
2025-11-13 21:34:42 +0200

    Completed assignment

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,37 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+        int N = Integer.parseInt(args[0]);
+        String mode = args[1];
+		int steps = 1;
+		boolean isVMode = mode.equals("v"); 
+		
+        for (int seed = 1; seed <= N; seed++) {
+            int current = seed;
+
+            if (isVMode) {
+                System.out.print(current + " ");
+            }
+
+            while (current != 1) {
+                if (current % 2 == 0) {
+                    current = current / 2;
+                } else {
+                    current = 3 * current + 1;
+                }
+
+                steps++;
+
+                if (isVMode) {
+                    System.out.print(current + " ");
+                }
+            }
+
+            if (isVMode) {
+                System.out.println("(" + steps + ")");
+            }
+        }
+
+        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+    }
 }
